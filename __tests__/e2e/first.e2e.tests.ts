9aace0e97961e7240e09a1067e5070adc44ceb42
Klim Androsov <kfasf@list.ru>
2023-11-14 21:22:30 +0400

    first commit

---
+++ __tests__/e2e/first.e2e.tests.ts
@@ -0,0 +1,152 @@
+import request from "supertest";
+import { VideoType, app } from "../../src/settings";
+describe("/videos", () => {
+  let video: VideoType;
+
+  beforeAll(async () => {
+    await request(app).delete("/all-data");
+  });
+  it("Get status 200 and found empty videos array", async () => {
+    await request(app).get("/videos").expect(200, []);
+  });
+  it("Get status 404 and not found 1 video by id", async () => {
+    await request(app).get("/videos/143424234246").expect(404);
+  });
+  it("Post status 400 and not created 1 video with uncorrect title and author", async () => {
+    const result = await request(app)
+      .post("/videos/")
+      .send({ title: 123, author: 321 });
+    expect(result.status).toBe(400);
+    expect(result.body).toEqual({
+      errorsMessages: [
+        {
+          message: expect.any(String),
+          field: "title",
+        },
+        { message: expect.any(String), field: "author" },
+      ],
+    });
+  });
+  it("Post status 201 and create 1 correct title and author", async () => {
+    const result = await request(app)
+      .post("/videos/")
+      .send({ title: "hello", author: "world" });
+    expect(result.status).toBe(201);
+    video = result.body;
+    expect(result.body).toEqual({
+      id: expect.any(Number),
+      title: "hello",
+      author: "world",
+      canBeDownloaded: false,
+      minAgeRestriction: null,
+      createdAt: expect.any(String),
+      publicationDate: expect.any(String),
+      availableResolutions: expect.any(Array),
+    });
+
+    expect(result.body.createdAt < result.body.publicationDate).toBe(true);
+
+    //expect(result.body.availableResolutions[0]).toBe("P144");
+  });
+  it("Put status 404 and not found uncorrect updated video", async () => {
+    const result = await request(app)
+      .put("/videos/" + 11111111111)
+      .send({
+        title: "string",
+        author: "string",
+        canBeDownloaded: true,
+        minAgeRestriction: 11,
+        createdAt: "string",
+        publicationDate: "string",
+        availableResolutions: ["P144"],
+      })
+      .expect(404);
+  });
+  it("Get status 200 and founded 1 video", async () => {
+    const res = await request(app).get("/videos").expect(200);
+    expect(res.body.length).toBe(1);
+  });
+  it("Post status 201 and created 1 video by id", async () => {
+    const result = await request(app)
+      .post("/videos/")
+      .send({ title: "hello", author: "world" });
+    video = result.body;
+
+    expect(result.status).toBe(201);
+
+    expect(result.body).toEqual({
+      id: expect.any(Number),
+      author: "world",
+      title: "hello",
+      canBeDownloaded: false,
+      minAgeRestriction: null,
+      createdAt: expect.any(String),
+      publicationDate: expect.any(String),
+      availableResolutions: expect.any(Array),
+    });
+  });
+  it("Get status 200 and 2 founded videos", async () => {
+    const res = await request(app).get("/videos").expect(200);
+    expect(res.body.length).toBe(2);
+  });
+  it("Get status 200 and 1 founded video by id", async () => {
+    const result = await request(app)
+      .get("/videos/" + video.id)
+      .expect(200);
+    expect(result.body).toEqual(video);
+  });
+  it("Put status 204 and 1 updated video by id", async () => {
+    const a = await request(app)
+      .put(`/videos/${video.id}`)
+      .send({
+        title: "string",
+        author: "string",
+        canBeDownloaded: true,
+        minAgeRestriction: 11,
+        publicationDate: "string",
+        availableResolutions: ["P144"],
+      })
+      .expect(204);
+
+    const result = await request(app)
+      .get("/videos/" + video.id)
+      .expect(200);
+
+    expect(result.body).toEqual({
+      id: video.id,
+      title: "string",
+      author: "string",
+      canBeDownloaded: true,
+      minAgeRestriction: 11,
+      createdAt: video.createdAt,
+      publicationDate: "string",
+      availableResolutions: ["P144"],
+    });
+  });
+  it("Put status 400 and not updated video with uncorrect title and author", async () => {
+    const result = await request(app)
+      .put("/videos/" + video.id)
+      .send({
+        title: 123,
+        author: 321,
+      });
+    expect(result.status).toBe(400);
+    expect(result.body).toEqual({
+      errorsMessages: [
+        {
+          message: expect.any(String),
+          field: "title",
+        },
+        { message: expect.any(String), field: "author" },
+      ],
+    });
+  });
+  it("Delete status 204 and delete 1 video by id", async () => {
+    const result = await request(app)
+      .delete("/videos/" + video.id)
+      .expect(204);
+  });
+  it("Delete status 404 and not deleted 1 video by id", async () => {
+    await request(app).delete("/videos/1111111111111111").expect(404);
+  });
+});
---
+++ src/settings.ts
@@ -0,0 +1,267 @@
+import express, { Request, Response, request, response } from "express";
+
+export const app = express();
+
+app.use(express.json());
+
+type RequestWithParams<P> = Request<P, {}, {}, {}>;
+
+type RequestWithBody<B> = Request<{}, {}, B, {}>;
+
+type RequestWithBodyAndParams<P, B> = Request<P, {}, B, {}>;
+
+type CreateVideoDto = {
+  title: string;
+  author: string;
+  availableResolutions: typeof availableResolutions;
+};
+
+type ErrorType = {
+  errorsMessages: ErorMessageType[];
+};
+
+type ErorMessageType = {
+  field: string;
+  message: string;
+};
+
+const availableResolutions = [
+  "P144",
+  "P240",
+  "P360",
+  "P480",
+  "P720",
+  "P1080",
+  "P1440",
+  "P2160",
+];
+
+export type VideoType = {
+  id: number;
+  title: string;
+  author: string;
+  canBeDownloaded: boolean;
+  minAgeRestriction: number | null;
+  createdAt: string;
+  publicationDate: string;
+  availableResolutions: typeof availableResolutions;
+};
+
+const videos: VideoType[] = [
+  {
+    id: 1,
+    title: "string",
+    author: "string",
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: "2023-11-08T12:16:55.998Z",
+    publicationDate: "2023-11-08T12:16:55.998Z",
+    availableResolutions: ["P144"],
+  },
+];
+
+app.get("/videos", (req: Request, res: Response) => {
+  res.status(200).send(videos);
+});
+
+type Params = {
+  id: string;
+};
+
+app.get("/videos/:id", (req: RequestWithParams<Params>, res: Response) => {
+  const id = +req.params.id;
+
+  const video = videos.find((v) => v.id == id);
+
+  if (!video) {
+    res.sendStatus(404);
+    return;
+  }
+  res.send(video);
+});
+
+app.post("/videos", (req: RequestWithBody<CreateVideoDto>, res: Response) => {
+  let errors: ErrorType = {
+    errorsMessages: [],
+  };
+
+  let { title, author, availableResolutions } = req.body;
+
+  if (typeof title !== "string") {
+    errors.errorsMessages.push({ message: "Invalid title", field: "title" });
+  } else if (title.trim().length < 1 || title.trim().length > 40) {
+    errors.errorsMessages.push({ message: "Invalid title", field: "title" });
+  }
+
+  if (typeof author !== "string") {
+    errors.errorsMessages.push({ message: "Invalid title", field: "author" });
+  } else if (author.trim().length < 1 || author.trim().length > 20) {
+    errors.errorsMessages.push({ message: "Invalid author", field: "author" });
+  }
+  if (Array.isArray(availableResolutions)) {
+    availableResolutions.map((r) => {
+      !availableResolutions.includes(r) &&
+        errors.errorsMessages.push({
+          message: "Invalid availableResolutions",
+          field: "availableResolutions",
+        });
+    });
+  } else {
+    availableResolutions = [];
+  }
+
+  if (errors.errorsMessages.length) {
+    res.status(400).send(errors);
+    return;
+  }
+
+  const createdAt = new Date();
+  const puiblicationDate = new Date();
+
+  puiblicationDate.setDate(createdAt.getDate() + 1);
+
+  const newVideo = {
+    id: +new Date(),
+    canBeDownloaded: false,
+    minAgeRestriction: null,
+    createdAt: createdAt.toISOString(),
+    publicationDate: puiblicationDate.toISOString(),
+    title,
+    author,
+    availableResolutions,
+  };
+
+  videos.push(newVideo);
+
+  res.status(201).send(newVideo);
+});
+
+type UpdateVideoDto = {
+  id: number;
+  title: string;
+  author: string;
+  availableResolutions: typeof availableResolutions;
+  canBeDownloaded: boolean;
+  minAgeRestriction: number | null;
+  publicationDate: string;
+};
+
+app.put(
+  "/videos/:id",
+  (req: RequestWithBodyAndParams<Params, UpdateVideoDto>, res: Response) => {
+    const id = +req.params.id;
+
+    let errors: ErrorType = {
+      errorsMessages: [],
+    };
+
+    let {
+      title,
+      author,
+      availableResolutions,
+      canBeDownloaded,
+      minAgeRestriction,
+      publicationDate,
+    } = req.body;
+
+    if (
+      !title ||
+      typeof title !== "string" ||
+      title.trim().length < 1 ||
+      title.trim().length > 40
+    ) {
+      errors.errorsMessages.push({ message: "Invalid title", field: "title" });
+    }
+
+    if (
+      !author ||
+      typeof author !== "string" ||
+      author.trim().length < 1 ||
+      author.trim().length > 20
+    ) {
+      errors.errorsMessages.push({
+        message: "Invalid author",
+        field: "author",
+      });
+    }
+    if (Array.isArray(availableResolutions)) {
+      availableResolutions.map((r) => {
+        !availableResolutions.includes(r) &&
+          errors.errorsMessages.push({
+            message: "Invalid availableResolutions",
+            field: "availableResolutions",
+          });
+      });
+    } else {
+      availableResolutions = [];
+    }
+    if (typeof canBeDownloaded == "undefined") {
+      canBeDownloaded = false;
+    }
+
+    if (
+      typeof minAgeRestriction !== "undefined" &&
+      typeof minAgeRestriction == "number"
+    ) {
+      minAgeRestriction < 1 ||
+        (minAgeRestriction > 18 &&
+          errors.errorsMessages.push({
+            message: "Invalid minAgeRestriction",
+            field: "minAgeRestriction",
+          }));
+    } else {
+      minAgeRestriction = null;
+    }
+    if (errors.errorsMessages.length) {
+      res.status(400).send(errors);
+      return;
+    }
+
+    //const videoIndex = videos.findIndex((v) => v.id == id);
+
+    const video = videos.find((v) => v.id == id);
+
+    if (!video) {
+      res.sendStatus(404);
+      return;
+    }
+
+    video.author = author;
+    video.title = title;
+    video.canBeDownloaded = canBeDownloaded;
+    video.minAgeRestriction = minAgeRestriction;
+    video.availableResolutions = availableResolutions;
+    video.publicationDate = publicationDate;
+
+    //const updatedItem = {
+    // ...video,
+    //  canBeDownloaded,
+    // minAgeRestriction,
+    //  title,
+    //  author,
+    //  availableResolutions,
+    //  publicationDate: publicationDate
+    //    ? publicationDate
+    //    : video.publicationDate,
+    //  };
+    // videos.splice(videoIndex, 1, updatedItem);
+    console.log(req.body);
+    return res.sendStatus(204);
+  }
+);
+
+app.delete("/all-data", (req: Request, res: Response) => {
+  videos.length = 0;
+  res.sendStatus(204);
+});
+
+app.delete("/videos/:id", (req: RequestWithParams<Params>, res: Response) => {
+  const id = +req.params.id;
+  const videoIndex = videos.findIndex((v) => v.id == id);
+  if (videoIndex == -1) {
+    res.sendStatus(404);
+    return;
+  }
+  videos.splice(videoIndex, 1);
+  res.sendStatus(204);
+});
